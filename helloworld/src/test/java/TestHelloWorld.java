a469d49ff8ce0407920a632e388785656fd009d7
RaghavPal <raghav9901@gmail.com>
2019-11-22 05:54:17 +0000

    first project commit

---
+++ helloworld/src/test/java/TestHelloWorld.java
@@ -0,0 +1,14 @@
+import org.testng.annotations.Test;
+
+public class TestHelloWorld {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("  Hello World ...!\n This is a demo....");
+	}
+
+	@Test
+	public void testrunTest() {
+		System.out.println(" ... This is a test ...");
+	}
+}
